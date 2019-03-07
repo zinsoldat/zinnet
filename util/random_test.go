71978ae7d2d20ca9ae90021dbeac82dbef4f336c
Rene Schmitt <rene.schmitt01@sap.com>
2019-03-07 08:43:09 +0100

    test: add unit test for random string length

---
+++ util/random_test.go
@@ -0,0 +1,16 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/zinsoldat/zinnet/util"
+)
+
+func TestRandString(t *testing.T) {
+	const stringLength = 5
+	randomString := util.RandString(stringLength)
+
+	if len(randomString) != stringLength {
+		t.Errorf("the random string should have a length of %d but it has a length of %d", stringLength, len(randomString))
+	}
+}
